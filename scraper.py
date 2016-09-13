df869006cdfcc9c846921a3317e2b50500fbab30
chris48s <c.mshaw@yahoo.co.uk>
2016-09-13 20:29:00 +0100

    update to match new base scraper format

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from arcgis_scraper import scrape
+from dc_base_scrapers.arcgis_scraper import ArcGisScraper
 
 
 stations_url = "http://maps.wolverhampton.gov.uk/arcgis/rest/services/PollingDistrictsStations2016/MapServer/1/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
@@ -6,5 +6,7 @@ districts_url = "http://maps.wolverhampton.gov.uk/arcgis/rest/services/PollingDi
 council_id = 'E08000031'
 
 
-scrape(stations_url, council_id, 'utf-8', 'stations')
-scrape(districts_url, council_id, 'utf-8', 'districts')
+stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
